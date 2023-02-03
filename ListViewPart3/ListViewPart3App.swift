c365a50bc83ea0324346b1a660de96964e2ccf43
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2023-02-03 17:33:55 +0900

    Initial Commit

---
+++ ListViewPart3/ListViewPart3App.swift
@@ -0,0 +1,17 @@
+//
+//  ListViewPart3App.swift
+//  ListViewPart3
+//
+//  Created by 渡邊魁優 on 2023/02/03.
+//
+
+import SwiftUI
+
+@main
+struct ListViewPart3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
